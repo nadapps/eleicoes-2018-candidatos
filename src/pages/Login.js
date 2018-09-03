8f77783fa10e10116638a5da995fac303cb0e4ce
André Lins <andre.lins@accenture.com>
2018-09-02 23:02:05 -0300

    Image Background

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TextInput, Text, AsyncStorage, ToastAndroid, Picker, View } from 'react-native';
+import { Text, AsyncStorage, ToastAndroid, Picker, View, ImageBackground } from 'react-native';
 import { Button } from 'react-native-elements';
 import { StackActions, NavigationActions } from 'react-navigation';
 import { estados } from '../constants';
@@ -42,29 +42,31 @@ export default class Login extends React.Component {
 
     render() {
         return (
-            <Content style={{padding:20}}>
-                <Text style={{color:"white", fontWeight:"bold", fontSize:30, textAlign:"center", padding:10, marginBottom:40, marginTop:40}}>Bem Vindo ao aplicativo Eleições 2018 !!!</Text>
-                <View style={{height: 50, borderColor: 'white', borderWidth: 1, borderRadius:5, marginTop:20}}>
-                    <Picker
-                        selectedValue={this.state.estado}
-                        style={{ color:"white"}}
-                        itemStyle={{ fontSize:40 }}
-                        onValueChange={(itemValue) => this.setState({estado: itemValue})}>
-                        <Picker.Item enabled={false} label="Escolha o Estado" value="" />
-                        {
-                            estados.map((estado) =>{
-                                return <Picker.Item key={estado.estadoabrev} label={estado.estado} value={estado.estadoabrev} />
-                            })
-                        }
-                    </Picker>
-                </View>
-                <Button
-                    containerViewStyle={{marginLeft:0, paddingLeft: 0, marginTop:20, width:"100%"}}
-                    raised
-                    backgroundColor={colors.white}
-                    color={colors.black}
-                    onPress={() => this.entrar()}
-                    title='Entrar' />
+            <Content>
+                <ImageBackground source={require('../../assets/img/brasil_branco.png')} style={{width: '100%', height: '100%', padding:20}}>
+                    <Text style={{color:"white", fontWeight:"bold", fontSize:30, textAlign:"center", padding:10, marginBottom:40, marginTop:40}}>Bem Vindo ao aplicativo Eleições 2018 !!!</Text>
+                        <View style={{height: 50, borderColor: 'white', borderWidth: 1, borderRadius:5, marginTop:20}}>
+                            <Picker
+                                selectedValue={this.state.estado}
+                                style={{ color:"white"}}
+                                itemStyle={{ fontSize:40 }}
+                                onValueChange={(itemValue) => this.setState({estado: itemValue})}>
+                                <Picker.Item enabled={false} label="Escolha o Estado" value="" />
+                                {
+                                    estados.map((estado) =>{
+                                        return <Picker.Item key={estado.estadoabrev} label={estado.estado} value={estado.estadoabrev} />
+                                    })
+                                }
+                            </Picker>
+                        </View>
+                        <Button
+                            containerViewStyle={{marginLeft:0, paddingLeft: 0, marginTop:20, width:"100%"}}
+                            raised
+                            backgroundColor={colors.white}
+                            color={colors.black}
+                            onPress={() => this.entrar()}
+                            title='Entrar' />
+                </ImageBackground>
             </Content>
         );
     }
