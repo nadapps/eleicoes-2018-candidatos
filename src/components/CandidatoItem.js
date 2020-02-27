f306876b3ce101bfd8830f3d10b302cab35def37
André Lins <andrelucas01@hotmail.com>
2020-02-27 18:19:40 -0300

    Refatoração em CandidatoImte e NumeroUrna

--- src/components/CandidatoItem.js
+++ src/components/CandidatoItem.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { View, Text, TouchableNativeFeedback } from 'react-native';
 import ImageProgress from 'react-native-image-progress';
@@ -7,124 +7,89 @@ import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons';
 
 import colors from '../core/colors';
 
-class CandidatoItem extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  onPress() {}
-
-  render() {
-    if (this.props.candidato.descricaoSituacao !== 'Indeferido') {
-      return (
-        <TouchableNativeFeedback
-          onPress={() => this.props.onPress(this.props.candidato)}
+const CandidatoItemComponent = ({ candidato, onPress, last, index }) => {
+  if (candidato.descricaoSituacao !== 'Indeferido') {
+    return (
+      <TouchableNativeFeedback onPress={() => onPress(candidato)}>
+        <View
+          style={[
+            !last
+              ? {
+                  borderBottomColor: colors.greyNew,
+                  borderBottomWidth: 1,
+                  backgroundColor: colors.white
+                }
+              : {
+                  backgroundColor: colors.white,
+                  borderBottomLeftRadius: 15,
+                  borderBottomRightRadius: 15
+                },
+            index == 0
+              ? { borderTopLeftRadius: 15, borderTopRightRadius: 15 }
+              : {}
+          ]}
         >
-          <View
-            style={[
-              !this.props.last
-                ? {
-                    borderBottomColor: colors.greyNew,
-                    borderBottomWidth: 1,
-                    backgroundColor: colors.white
-                  }
-                : {
-                    backgroundColor: colors.white,
-                    borderBottomLeftRadius: 15,
-                    borderBottomRightRadius: 15
-                  },
-              this.props.index == 0
-                ? { borderTopLeftRadius: 15, borderTopRightRadius: 15 }
-                : {}
-            ]}
-          >
-            <ImageProgress
-              indicator={Circle}
-              style={{
-                position: 'absolute',
-                top: 0,
-                left: 0,
-                zIndex: 100,
-                width: 40,
-                height: 40,
-                marginTop: 9,
-                marginLeft: 9
-              }}
-              imageStyle={{
-                resizeMode: 'cover',
-                borderRadius: 100
-              }}
-              source={{
-                uri: 'https://i.stack.imgur.com/34AD2.jpg'
-                // 'http://35.227.86.242/eleicoes/candidatos/' +
-                // this.props.candidato.id +
-                // '.jpg'
-              }}
-              indicatorProps={{
-                size: 20,
-                borderWidth: 0,
-                color: colors.accent
-              }}
+          <ImageProgress
+            indicator={Circle}
+            style={style.imageProgress}
+            imageStyle={{ resizeMode: 'cover', borderRadius: 100 }}
+            source={{ uri: 'https://i.stack.imgur.com/34AD2.jpg' }}
+            indicatorProps={{ size: 20, borderWidth: 0, color: colors.accent }}
+          />
+          <View style={style.text}>
+            <Text numberOfLines={1} style={style.title}>
+              {candidato.nomeCompleto}
+            </Text>
+            <Text numberOfLines={1} style={style.description}>
+              {`${candidato.partido.sigla} - ${candidato.nomeColigacao}`}
+            </Text>
+            <SimpleLineIcons
+              style={{ position: 'absolute', right: 15, top: 22 }}
+              name="arrow-right"
+              size={15}
+              color={colors.gray}
             />
-            <View
-              style={{
-                flexDirection: 'column',
-                padding: 10,
-                marginLeft: 50,
-                zIndex: 0
-              }}
-            >
-              <Text
-                numberOfLines={1}
-                style={{
-                  textAlign: 'left',
-                  color: colors.black,
-                  fontSize: 15,
-                  width: '90%'
-                }}
-              >
-                {this.props.candidato.nomeCompleto}
-              </Text>
-              <Text
-                numberOfLines={1}
-                style={{
-                  textAlign: 'left',
-                  color: colors.gray,
-                  fontSize: 12,
-                  fontWeight: 'bold',
-                  width: '90%'
-                }}
-              >
-                {this.props.candidato.partido.sigla +
-                  ' - ' +
-                  this.props.candidato.nomeColigacao}
-              </Text>
-              <SimpleLineIcons
-                style={{ position: 'absolute', right: 15, top: 22 }}
-                name="arrow-right"
-                size={15}
-                color={colors.gray}
-              />
-            </View>
           </View>
-        </TouchableNativeFeedback>
-      );
-    } else {
-      return <View />;
-    }
+        </View>
+      </TouchableNativeFeedback>
+    );
+  } else {
+    return <View />;
   }
-}
+};
 
-CandidatoItem.propTypes = {
+const style = {
+  imageProgress: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    zIndex: 100,
+    width: 40,
+    height: 40,
+    marginTop: 9,
+    marginLeft: 9
+  },
+  text: { flexDirection: 'column', padding: 10, marginLeft: 50, zIndex: 0 },
+  title: { textAlign: 'left', color: colors.black, fontSize: 15, width: '90%' },
+  description: {
+    textAlign: 'left',
+    color: colors.gray,
+    fontSize: 12,
+    fontWeight: 'bold',
+    width: '90%'
+  }
+};
+
+CandidatoItemComponent.propTypes = {
   candidato: PropTypes.object,
   last: PropTypes.bool,
   onPress: PropTypes.func,
   index: PropTypes.number
 };
 
-CandidatoItem.defaultProps = {
+CandidatoItemComponent.defaultProps = {
   candidato: {},
   last: false
 };
 
-export default CandidatoItem;
+export default CandidatoItemComponent;
--- src/components/NumeroUrna.js
+++ src/components/NumeroUrna.js
@@ -1,56 +1,38 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { View, Text } from 'react-native';
 import colors from '../core/colors';
 
-class NumeroUrna extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <View
-        style={
-          !this.props.style
-            ? {
-                flexDirection: 'row',
-                alignItems: 'center',
-                justifyContent: 'center'
-              }
-            : [
-                {
-                  flexDirection: 'row',
-                  alignItems: 'center',
-                  justifyContent: 'center'
-                },
-                this.props.style
-              ]
-        }
+const NumeroUrna = ({ style, numero, fontSize, padding }) => (
+  <View
+    style={{
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'center',
+      ...style
+    }}
+  >
+    {numero.split('').map((numero, index) => (
+      <Text
+        key={index}
+        style={{
+          color: colors.black,
+          fontWeight: 'bold',
+          fontSize: fontSize,
+          padding: padding - 2,
+          paddingLeft: padding,
+          paddingRight: padding,
+          borderWidth: 0.5,
+          borderColor: colors.black,
+          marginLeft: padding - 2,
+          backgroundColor: colors.white
+        }}
       >
-        {this.props.numero.split('').map((numero, index) => (
-          <Text
-            key={index}
-            style={{
-              color: colors.black,
-              fontWeight: 'bold',
-              fontSize: this.props.fontSize,
-              padding: this.props.padding - 2,
-              paddingLeft: this.props.padding,
-              paddingRight: this.props.padding,
-              borderWidth: 0.5,
-              borderColor: colors.black,
-              marginLeft: this.props.padding - 2,
-              backgroundColor: colors.white
-            }}
-          >
-            {numero}
-          </Text>
-        ))}
-      </View>
-    );
-  }
-}
+        {numero}
+      </Text>
+    ))}
+  </View>
+);
 
 NumeroUrna.propTypes = {
   numero: PropTypes.string,
